3f2e279726abefea189e418e5674354baba6a9b9
PoznyakAnn <148707516+PoznyakAnn@users.noreply.github.com>
2025-03-04 00:39:49 +0300

    Delete product.py

--- product.py
+++
@@ -1,9 +0,0 @@
-class Product:
-    def __init__(self, name: str, price: float, shelf_life: int) -> None:
-        self.name = name
-        self.price = price
-        self.shelf_life = shelf_life
-        self.discounted_price = price
-
-    def apply_discount(self, discount: float):
-        self.discounted_price = self.price * (1 - discount / 100)
